e037faec36ca8240e707586e1b57ae146b28e7be
saiashish9 <saiashish3760@gmail.com>
2024-01-11 12:56:35 +0530

    k

--- src/constants/translations.js
+++ src/constants/translations.js
@@ -2,10 +2,94 @@ const translations = [
   {
     text: "deadlineBannerText",
     en: "The deadline to file a 2022 tax return has passed",
+    es: "Ya pasó el plazo para presentar la declaración de impuestos de 2022",
   },
   {
-    text: "434",
-    en: "78",
+    text: "deadlineBannerSubText",
+    en: (
+      <span>
+        <span>
+          If you started your 2022 tax return with TurboTax, you have until Oct
+          31 at 9PM PST to file your return online. You may owe a late filing
+          fee for
+        </span>
+        <br />
+        filing after Oct 16
+      </span>
+    ),
+    es: (
+      <span>
+        <span>
+          Si comenzó su declaración de impuestos de 2022 con TurboTax, tiene
+          hasta el 31 de octubre a las 9 p.m. PST para presentar su declaración
+          en línea. Es posible que deba un cargo por presentación tardía por
+        </span>
+        <span>presentación después del 16 de octubre</span>
+      </span>
+    ),
+  },
+  {
+    text: "learnMore",
+    en: "Learn more",
+    es: "Aprende más",
+  },
+  {
+    text: "fileNow",
+    en: "File Now",
+    es: "Archivar ahora",
+  },
+  {
+    text: "title1",
+    en: "File your own taxes",
+    es: "Presenta tus propios",
+  },
+  {
+    text: "title2",
+    en: "with confidence",
+    es: (
+      <p>
+        impuestos con <br /> confianza
+      </p>
+    ),
+  },
+  {
+    text: "guaranteed",
+    en: "guaranteed",
+    es: "garantizado",
+  },
+  {
+    text: "subTitle",
+    en: (
+      <span>
+        As Americas #1 tax prep provider, TurboTax makes it <br />
+        quick and easy to get your max refund and 100% <br />
+        accurate calculations
+      </span>
+    ),
+    es: (
+      <span>
+        Como proveedor número uno de preparación de <br />
+        impuestos en Estados Unidos, <br />
+        TurboTax lo hace Rápido y fácil <br />
+        de obtener su reembolso máximo y <br />
+        100% cálculos precisos
+      </span>
+    ),
+  },
+  {
+    text: "howItWorks",
+    en: "See how it works",
+    es: "Mira como funciona",
+  },
+  {
+    text: "startForFree",
+    en: "Start for free",
+    es: "Empieza gratis"
+  },
+  {
+    text: "payOnly",
+    en: "Pay only when you file",
+    es: <p>Pague solo cuando <br /> presente la solicitud</p>
   }
 ];
 
